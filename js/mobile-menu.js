3822f85a9111844099b0b25a572b5c27330f5a8c
Vasiliy-Masterov <88328749+Vasiliy-Masterov@users.noreply.github.com>
2021-11-05 23:32:28 +0200

    Add files via upload

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,1 +1,21 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.mobile-menu__toggle'),
+    closeMenuBtn: document.querySelector('.mobile-menu__button-close'),
+    menu: document.querySelector('.backdrop'),
+    menuItem: document.querySelector('.mobile-menu_container')
+  };
 
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menu.addEventListener('click', removeMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+  function removeMenu(e) {
+    if (e.target === refs.menu) {
+      refs.menu.classList.add('is-hidden');
+    }
+  }
+})();
